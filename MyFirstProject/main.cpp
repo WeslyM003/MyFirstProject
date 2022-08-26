192d50c495fbef9e2fa1153a27ba3da4d1b6a7ed
WeslyM003 <maulsbywes@gmail.com>
2022-08-26 10:30:59 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,20 @@
+/*********************
+Wesly Maulsby
+C++ Fall 2022
+Due: Aug 31, 2022
+Lab 1 Intro to C++ Programming
+This lab is a demo of my abillity to work with an IDE and GitHub
+*********************/
+
+
+#include <iostream>		//Includes iostream library
+
+
+int main() 
+{
+	
+	std::cout << "Hello World!" << " Blue";
+
+
+	return 0;
+}
